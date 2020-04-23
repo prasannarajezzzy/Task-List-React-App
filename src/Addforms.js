a9f4f44e94718f4255b785107b76a13fd3f0413a
prasanna <prasanna.rajezzy@gmal.com>
2020-04-23 13:28:08 +0530

    done

---
+++ src/Addforms.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+
+
+class AddTodo extends Component{
+	state={
+		content:''
+	}
+	handleChange=(e)=>{
+		this.setState({
+			content:e.target.value
+		})
+
+
+
+	}
+	handleSubmit=(e)=>{
+		e.preventDefault();
+		console.log(this.state.content)
+		this.props.addTodo(this.state);
+		this.setState({
+			content: ''
+		})
+	}
+	render(){
+		return (
+			<div>
+			<form onSubmit={this.handleSubmit}>
+			<label>Add A task</label>
+			<input type="text" onChange={this.handleChange}/>
+			</form>
+			</div>)
+	}
+
+
+
+}
+export default AddTodo
